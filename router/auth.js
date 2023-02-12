26cfaecd06649e966b24fa3a687b60f5c5d78f24
Jil <jilpatel2105@gmail.com>
2023-02-12 16:22:59 +0530

    jh

--- router/auth.js
+++ router/auth.js
@@ -5,6 +5,9 @@ const bcrypt = require("bcryptjs");
 const authenticate=require("../authenticate/authenticate.js")
 
 //asnyc-await
+router.get("/", (req, res) => {
+    res.send("hello form me")
+})
 
 router.post("/register", async(req, res) => {
     const { name, email, phone, work, password, cpassword } = req.body;
